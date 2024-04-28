7ebe0c238b5d245bcd544a58217b129282f0b294
xxxsjan <626653354@qq.com>
2024-04-28 18:16:32 +0800

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,22 @@ import { useEffect, useState } from "react"
 function Footer(props) {
   return (
     <div className="popup-footer">
-      <span>© 2024 xxxsjan</span>
+      <span>© 2024 xxxsjan&nbsp;</span>
+      <span>&nbsp;</span>
 
-      <span>
-        power by
-        <a
-          target="_blank"
-          href="https://developer.chrome.com/docs/extensions/develop?hl=zh-cn">
-          by google extensions
-        </a>
-        <a
-          href="https://docs.plasmo.com"
-          target="_blank"
-          className="text-right">
-          plasmo
-        </a>
-      </span>
+      <span>power by </span>
+      <span>&nbsp;</span>
+
+      {/* <a
+        target="_blank"
+        href="https://developer.chrome.com/docs/extensions/develop?hl=zh-cn">
+        google extensions
+      </a>
+      <span>&nbsp;/&nbsp;</span> */}
+
+      <a href="https://docs.plasmo.com" target="_blank" className="text-right">
+        plasmo
+      </a>
     </div>
   )
 }
